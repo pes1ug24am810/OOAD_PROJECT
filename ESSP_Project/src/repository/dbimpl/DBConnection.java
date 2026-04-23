f979aa52d2d969f4bd8bc2c34dffdcafe322a5b5
Mithun G <pes1ug24am810@pesu.pes.edu>
2026-04-24 01:24:50 +0530

    Update DBConnection.java

--- ESSP_Project/src/repository/dbimpl/DBConnection.java
+++ ESSP_Project/src/repository/dbimpl/DBConnection.java
@@ -5,12 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    // The "hrms.db" file should be in your main project folder
+    // The "hrms.db" file should be in our main project folder
     private static final String URL = "jdbc:sqlite:hrms.db";
 
     public static Connection getConnection() throws SQLException {
         try {
-            // This loads the SQLite driver from the JAR file you added
+            // This loads the SQLite driver from the JAR file we added
             Class.forName("org.sqlite.JDBC");
             return DriverManager.getConnection(URL);
         } catch (ClassNotFoundException e) {
